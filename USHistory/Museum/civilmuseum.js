024c0cbeb9a6e5e61d388b05ac58f54bf3919988
ReMakeEver <evergenbyremake@gmail.com>
2016-08-18 13:29:19 -0600

    Update civilmuseum.js

--- USHistory/Museum/civilmuseum.js
+++ USHistory/Museum/civilmuseum.js
@@ -1,5 +1,5 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight*80% );
+renderer.setSize( window.innerWidth, window.innerHeight*.8 );
 document.body.appendChild( renderer.domElement );

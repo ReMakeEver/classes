42cb4349f4849b76871ddcd7406332a9e49a2c42
ReMakeEver <evergenbyremake@gmail.com>
2016-08-18 14:15:46 -0600

    Update civilmuseum.js

--- USHistory/Museum/civilmuseum.js
+++ USHistory/Museum/civilmuseum.js
@@ -1,15 +1,1 @@
-var menu = "<font size='12'>";menu += "<a href='javascript:enter()'>Begin Tour</a>";menu += "</font>";
 
-document.getElementById("menu").innerHTML = menu;
-
-var scene1 = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight*.8 );
-document.getElementById("canvas").appendChild( renderer.domElement );
-
-function render1(){
-  requestAnimationFrame( render );
-  renderer.render( scene1, camera );
-}
-function enter(){  render1(); }

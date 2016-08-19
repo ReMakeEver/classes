43a4a337ce6f3b92d80c80b41aae5ce2ed495c67
ReMakeEver <evergenbyremake@gmail.com>
2016-08-19 11:03:05 -0600

    Update civilmuseum.js

--- USHistory/Museum/civilmuseum.js
+++ USHistory/Museum/civilmuseum.js
@@ -1,46 +1,27 @@
-var motion = {
- airborne: false,
- position: new THREE.Vector3(), velocity: new THREE.Vector3(),
- rotation: new THREE.Vector2(), spinning: new THREE.Vector2()
-};
-motion.position.y = -150;
-var resetPlayer = function(){
- if( motion.position.y < -123 ){
-  motion.position.set( -2, 7.7, 25 );
-  motion.velociy.multiplyScalar( 0 );
- }
-};
-var keyboardControls = (function(){
- var keys = { W: 87,  A: 65, S: 83, D: 68, UP: 38, LT: 37, DN: 40, RT: 39 };
- var keysPressed = {};
- (function( watchedKeyCodes ){
-  var handler = function( down ){
-   return function( e ){
-    var index = watchedKeyCodes.indexOf( e.keyCode );
-    if( index >= 0 ){
-     keysPressed[watchedKeyCodes[index]] = down;e.preventDefault();
-    }
-   };
-  };
- })([
-  keys.W, keys.A, keys.S, keys.D, keys.UP, keys.LT, keys.DN, keys.RT
- ]);
- var forwart = new THREE.Vector3();
- var sideways = new THREE.Vector3();
- return function(){
-  if( !motion.airborne ){
-   var sx = keysPressed[keys.UP] ? 0.3 : ( keysPressed[keys.DN] ? -0.3 : 0 );
-   var sy = keysPressed[keys.LT] ? 0.3 : ( keysPressed[keys.RT] ? -0.3 : 0 );
-   if( Math.abs( sx ) >= Math.abs( motion.spinning.x ) ) motion.spinning.x = sx;
-   if( Math.abs( sy ) >= Math.abs( motion.spinning.y ) ) motion.spinning.y = sy;
-   forward.set( Math.sin( motion.position.y ), 0, Math.cos( motion.spinning.y ) );
-   sideways.set( forward.z, 0, -forward.x );
-   forward.multiplyScalar( keysPressed[keys.W] ? -0.1 : (keysPressed[keys.S] ? 0.1 : 0 ));
-   sideways.multiplyScalar( keysPressed[keys.A] ? -0.1 : (keysPressed[keys.D] ? 0.1 : 0 ));
-   var combined = forward.add( sideways );
-   if( Math.abs( combined.x ) >= Math.abs( motion.veloctiy.x ) ) motion.velocity.x = combined.x;
-   if( Math.abs( combined.y ) >= Math.abs( motion.velocity.y ) ) motion.velocity.y = combined.y;
-   if( Math.abs( combined.z ) >= Math.abs( motion.velocity.z ) ) motion.veloctiy.z = combined.z;
-  }
- };
-})();
+var renderer = new THREE.WebGLRenderer({ antialias : true });
+renderer.setPixelRatio( window.devicePixelRatio );
+var camera = new THREE.PerspectiveCamera( 60, 1, 0.1, 9000 );
+var scene = new THREE.Scene();
+scene.add( camera );
+scene.add( makeSkyBox([
+ 'textures/sky/px.jpg',
+ 'textures/sky/nx.jpg',
+ 'textures/sky/py.jpg',
+ 'textures/sky/ny.jpg',
+ 'textures/sky/pz.jpg',
+ 'textures/sky/nz.jpg'
+ ], 8000 ));
+
+function makeSkybox( urls, size ){
+ var skyboxCubemap = new THREE.CubeTextureLoader().load( urls );
+ skyboxCubemap.format = THREE.RGBFormat;
+ var skyboxShader = THREE.ShaderLib['cube'];
+ skyboxShader.uniforms['tCube'].value = skyboxCubemap;
+ return new THREE.Mesh(
+  new THREE.BoxGeometry( size, size, size ),
+  new THREE.ShadeMaterial({
+   fragmentShader: skyboxShader.fragmentShader, vertexShader: skyboxShader.vertexShader,
+   uniforms: skyboxShader.uniforms, depthWrite: false, side: THREE.BackSide
+  })
+ );
+}

8e0bb2b3af5aef58bb34984446c63d4d2aad5871
ReMakeEver <evergenbyremake@gmail.com>
2016-08-19 13:08:52 -0600

    Update game3.js

--- JavaScript/game3.js
+++ JavaScript/game3.js
@@ -1,7 +1,7 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 60, 1, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+var renderer = new THREE.WebGLRenderer({ antialias : true });
+renderer.setPixelRatio( window.devicePixelRatio );
 
 function makeSkybox( urls, size ){
  var skyboxCubemap = new THREE.CubeTextureLoader().load( urls );

143d3053a5f2e3c549c438c6432bfbf473e7392a
ReMakeEver <evergenbyremake@gmail.com>
2016-08-21 21:48:52 -0600

    Update threejs.js

--- threejs.js
+++ threejs.js
@@ -9,21 +9,15 @@ var geometry = new THREE.BoxGeometry( 1,1,1 );
 
 var material = new THREE.MeshNormalMaterial();
 
-var cube = new THREE.Mesh( geometry, material );var cube1 = new THREE.Mesh( geometry, material );
-cube1.visible = false;
+var cube = new THREE.Mesh( geometry, material );
 
-var group = new THREE.Group();
-group.add( cube );group.add( cube1 );group.remove( cube1 );
-group.children;
-group.parent;
-
-scene.add( group );
+scene.add( cube );
 camera.position.z = 5;
 
 var render = function(){
  requestAnimationFrame( render );
  cube.rotation.x += 0.1;
- cube.rotation.y += 0.1;
+ cube.rotation.y = THREE.Math.degToRad(45);
  renderer.render( scene, camera );
 };
 render();

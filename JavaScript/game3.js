4603b5e79472f27537f5b7b41f91e326104526e7
ReMakeEver <evergenbyremake@gmail.com>
2016-08-23 14:39:20 -0600

    Update game3.js

--- JavaScript/game3.js
+++ JavaScript/game3.js
@@ -5,6 +5,11 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( renderer.domElement );
 document.body.appendChild( renderer.domElement );
 
+var sky = new THREE.Sky();
+scene.add( sky.mesh );
+var sUni = sky.uniforms;
+sUni.turbidity.value = 20;sUni.reileigh.value = 2;sUni.mieCoefficient.value = .005;sUni.mieDeirectionalG = .8;sUni.luminance.value = 1;sUni.inclination.value=0.49;sUni.azimuth.value = .25;
+
 var render = function(){
  requestAnimationFrame( render );
  renderer.render( scene, camera );

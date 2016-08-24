f7da7517fb917c22a847acf6d4cdcf4ef5ad3005
ReMakeEver <evergenbyremake@gmail.com>
2016-08-24 08:38:40 -0600

    Update game3.js

--- JavaScript/game3.js
+++ JavaScript/game3.js
@@ -5,29 +5,6 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var sky = new THREE.Sky();
-
-function createSky(){
- scene.add( sky.mesh );
- var eC = {
-  turbidity: 20,
-  reileigh: 2,
-  mieCoefficient: 0.005,
-  mieDirectionalG: 0.8,
-  luminance: 1,
-  inclination: 0.49,
-  azimuth: 0.25,
-  sun: ! true
- };
- var uniforms = sky.uniforms;
- uniforms.turbidity.value = eC.turbidity;
- uniforms.reileigh.value = eC.reileigh;
- uniforms.luminance.value = eC.luminance;
- uniforms.mieCoefficient.value = eC.mieCoefficient;
- uniforms.mieDirectionalG.value = eC.mieDirectionalG;
- renderer.render( scene, camera );
-}
-
 var render = function(){
  requestAnimationFrame( render );
  createSky();

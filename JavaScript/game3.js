69a66802239892c9b8ff4c645779b8fb3743b57e
ReMakeEver <evergenbyremake@gmail.com>
2016-08-23 21:40:34 -0600

    Update game3.js

--- JavaScript/game3.js
+++ JavaScript/game3.js
@@ -2,7 +2,7 @@ var scene = new THREE.Scene();
 var aspect = window.innerWidth / window.innerHeight;
 var camera = new THREE.PerspectiveCamera( 75, aspect, 0.1, 1000 );camera.position.set( 0, 100, 2000)
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( renderer.domElement );
+renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var sky = new THREE.Sky();

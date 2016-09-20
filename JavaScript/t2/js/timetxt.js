b3e5c129626541953acb2594d08a10c275e7be92
ReMakeEver <evergenbyremake@gmail.com>
2016-09-20 13:11:06 -0600

    Add files via upload

---
+++ JavaScript/t2/js/timetxt.js
@@ -0,0 +1,35 @@
+/*
+   New Perspectives on JavaScript
+   Tutorial 2
+   Case Problem 4
+
+   Author: 
+   Date:   
+
+   Function List:
+   dayDiff(start, stop)
+      Calculates the number of days, rounded down the next lowest integer, 
+      between a starting date and stopping date.
+
+   hoursLeft(time)
+      Calculates the number of hours left in the current day rounded down to
+      the next lowest integer.
+
+   minutesLeft(time)
+      Calculates the number of minutes left in the current hour rounded down 
+      to the next lowest integer.
+
+   showDate(time)
+      Displays the value of the time object in the format:
+      mm/dd/yyyy
+
+   showTime(time)
+      Displays the value of the time object in the format:
+      hh:mm am/pm
+*/
+
+
+
+
+
+

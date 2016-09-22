0ba317481408d299347f1d267e6a87433259f305
ReMakeEver <evergenbyremake@gmail.com>
2016-09-22 14:16:45 -0600

    Update time.js

--- JavaScript/t2/js/time.js
+++ JavaScript/t2/js/time.js
@@ -1,6 +1,6 @@
 var one = "Monday";var two = "Tuesday";var three = "Wenesday";var four = "Thursday";var five = "Friday";var six = "Saturday";var seven = "Sunday";
 var jan = "January";var feb = "Febuary";var mar = "March";var apr = "April";var may = "May";var jun = "June";var jul = "July";var aug = "August";var sep = "September";var oct = "October";var nov = "November";var dec = "December";
-var am = "AM";var pm = "PM";
+var am = "AM";var pm = "PM";var string;
 function renew(){
  document.getElementById("theMonth").innerHTML = month();
  document.getElementById("theDay").innerHTML = day();
@@ -16,5 +16,6 @@ function day(){
 }
 function time(){
  var d = new Date();var n = d.toLocaleTimeString();var len = n.length;
- var string = n.substr(0,8)+" "+n.substr(len-2,len);
+ string = n.substr(0,8)+" "+n.substr(len-2,len);
+ return string;
 }

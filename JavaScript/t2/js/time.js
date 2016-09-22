8339f196a1417a0f8d1de5bd94b4640e41864e1a
ReMakeEver <evergenbyremake@gmail.com>
2016-09-22 14:16:13 -0600

    Update time.js

--- JavaScript/t2/js/time.js
+++ JavaScript/t2/js/time.js
@@ -1,17 +1,20 @@
 var one = "Monday";var two = "Tuesday";var three = "Wenesday";var four = "Thursday";var five = "Friday";var six = "Saturday";var seven = "Sunday";
-var min = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59];
-var hr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
 var jan = "January";var feb = "Febuary";var mar = "March";var apr = "April";var may = "May";var jun = "June";var jul = "July";var aug = "August";var sep = "September";var oct = "October";var nov = "November";var dec = "December";
 var am = "AM";var pm = "PM";
 function renew(){
  document.getElementById("theMonth").innerHTML = month();
  document.getElementById("theDay").innerHTML = day();
+ document.getElementById("theTime").innerHTML = time();
 }
 function month(){
  var month = new Date().getMonth();
- if(month=="0"){ return jan; }if(month=="1"){ return feb; }if(month=="2"){ return mar; }if(month=="3"){ return apr; }if(month=="4"){ return may; }if(month=="5"){ return jun; }if(month=="6"){ return jul; }
+ if(month=="0"){ return jan; }if(month=="1"){ return feb; }if(month=="2"){ return mar; }if(month=="3"){ return apr; }if(month=="4"){ return may; }if(month=="5"){ return jun; }if(month=="6"){ return jul; }if(month=="7"){ return aug; }if(month=="8"){ return sep; }if(month=="9"){ return oct; }if(month=="10"){ return nov; }if(month=="11"){ return dec; } 
 }
 function day(){
  var day = new Date().getDay();
  if(day=="0"){ return one; }if(day=="1"){ return two; }if(day=="2"){ return three; }if(day=="3"){ return four; }if(day=="4"){ return five; }if(day=="5"){ return six; }if(day=="6"){ return seven; }
+}
+function time(){
+ var d = new Date();var n = d.toLocaleTimeString();var len = n.length;
+ var string = n.substr(0,8)+" "+n.substr(len-2,len);
 }

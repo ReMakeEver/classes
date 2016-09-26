88a20c11a3c7468237f4b84556b1c1fc22beae51
ReMakeEver <evergenbyremake@gmail.com>
2016-09-26 09:05:30 -0600

    Update time.js

--- JavaScript/t2/js/time.js
+++ JavaScript/t2/js/time.js
@@ -16,5 +16,10 @@ function day(){
 }
 function time(){
  var d = new Date();var n = d.toLocaleTimeString();var len = n.length;
- return n.substr(0,8)+" "+n.substr(len-2,len);
+ if(len==33){
+  return n.substr(0,8)+" "+n.substr(len-2,len);
+ }
+ if(len==21){
+  return n.substr(0,4)+" "+n.substr(len-2,len);
+ }
 }

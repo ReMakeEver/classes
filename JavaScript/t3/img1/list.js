39dd0c835aa2c7b5cf61b21c513e62a1caa3f10a
ReMakeEver <evergenbyremake@gmail.com>
2016-10-25 13:49:48 -0600

    Add files via upload

---
+++ JavaScript/t3/img1/list.js
@@ -0,0 +1,378 @@
+/*
+   New Perspectives on JavaScript
+   Tutorial 3
+   Case Problem 1
+
+   Filename: list.js
+
+   Variable List:
+
+   firstName: Contains the first name of the contributors.
+   lastName:  Contains the last name of the contributors.
+   street:    Contains the contributor's street address
+   city:      Contains the contributor's city of residence
+   state:     Contains the contributor's state of residence
+   zip:       Contains the contributor's zip code.
+   amount:    Contains the amount of the contribution.
+   date:      Contains the date of the contribution.
+
+*/
+
+firstName = new Array();
+lastName = new Array();
+street = new Array();
+city = new Array();
+state= new Array();
+zip = new Array();
+amount = new Array();
+date = new Array()
+
+
+firstName[0]="Nina";
+lastName[0]="Largent";
+street[0]="88 Regal Lane";
+city[0]="Wheaton";
+state[0]="KY";
+zip[0]="89780";
+amount[0]=125;
+date[0]="2007-09-18";
+
+
+firstName[1]="Mike";
+lastName[1]="Hunt";
+street[1]="404 Barrow Street";
+city[1]="Drake";
+state[1]="KY";
+zip[1]="88841";
+amount[1]=75;
+date[1]="2007-09-18";
+
+
+firstName[2]="Monica";
+lastName[2]="Lang";
+street[2]="743 Stawlings Drive";
+city[2]="Youngston";
+state[2]="KY";
+zip[2]="89747";
+amount[2]=50;
+date[2]="2007-09-16";
+
+
+firstName[3]="William";
+lastName[3]="Mcknight";
+street[3]="102 Maple Lane";
+city[3]="Youngston";
+state[3]="KY";
+zip[3]="88660";
+amount[3]=150;
+date[3]="2007-09-15";
+
+
+firstName[4]="Latrina";
+lastName[4]="Hults";
+street[4]="750 Whitehall Road";
+city[4]="Drake";
+state[4]="KY";
+zip[4]="89664";
+amount[4]=250;
+date[4]="2007-09-14";
+
+
+firstName[5]="Danny";
+lastName[5]="Shamblin";
+street[5]="123 Smith Drive";
+city[5]="Delphi";
+state[5]="KY";
+zip[5]="88585";
+amount[5]=50;
+date[5]="2007-09-13";
+
+
+firstName[6]="Tina";
+lastName[6]="Ammons";
+street[6]="888 Evans Way";
+city[6]="Wheaton";
+state[6]="KY";
+zip[6]="89847";
+amount[6]=50;
+date[6]="2007-09-13";
+
+
+firstName[7]="Joanne";
+lastName[7]="Fine";
+street[7]="210 Bowling Terrace";
+city[7]="Wheaton";
+state[7]="KY";
+zip[7]="88632";
+amount[7]=125;
+date[7]="2007-09-11";
+
+
+firstName[8]="Charlotte";
+lastName[8]="Foulk";
+street[8]="109 South Road";
+city[8]="Youngston";
+state[8]="KY";
+zip[8]="88835";
+amount[8]=50;
+date[8]="2007-09-10";
+
+
+firstName[9]="Candice";
+lastName[9]="Alfaro";
+street[9]="108 Atwood Avenue";
+city[9]="Delphi";
+state[9]="KY";
+zip[9]="89611";
+amount[9]=400;
+date[9]="2007-09-08";
+
+
+firstName[10]="Kristi";
+lastName[10]="Laine";
+street[10]="512 North Lane";
+city[10]="Wheaton";
+state[10]="KY";
+zip[10]="88754";
+amount[10]=225;
+date[10]="2007-09-08";
+
+
+firstName[11]="Elisabeth";
+lastName[11]="Carbone";
+street[11]="381 Main Street";
+city[11]="Drake";
+state[11]="KY";
+zip[11]="89704";
+amount[11]=200;
+date[11]="2007-09-07";
+
+
+firstName[12]="James";
+lastName[12]="Larsen";
+street[12]="212 Rawlings Way";
+city[12]="Jasper";
+state[12]="KY";
+zip[12]="88646";
+amount[12]=125;
+date[12]="2007-09-07";
+
+
+firstName[13]="Ralph";
+lastName[13]="Thornton";
+street[13]="444 Smith Drive";
+city[13]="Delphi";
+state[13]="KY";
+zip[13]="89755";
+amount[13]=100;
+date[13]="2007-09-07";
+
+
+firstName[14]="Robin";
+lastName[14]="Witt";
+street[14]="78 Norland Pines";
+city[14]="Drake";
+state[14]="KY";
+zip[14]="88711";
+amount[14]=75;
+date[14]="2007-09-07";
+
+
+firstName[15]="Alex";
+lastName[15]="Ruiz";
+street[15]="102 Sunset Road";
+city[15]="Jasper";
+state[15]="KY";
+zip[15]="89614";
+amount[15]=50;
+date[15]="2007-09-06";
+
+
+firstName[16]="Callie";
+lastName[16]="Rudy";
+street[16]="3 Sunset Road";
+city[16]="Jasper";
+state[16]="KY";
+zip[16]="88576";
+amount[16]=50;
+date[16]="2007-09-06";
+
+
+firstName[17]="Michael";
+lastName[17]="Harrell";
+street[17]="125 Sunset Road";
+city[17]="Jasper";
+state[17]="KY";
+zip[17]="89711";
+amount[17]=50;
+date[17]="2007-09-06";
+
+
+firstName[18]="Edgar";
+lastName[18]="Morales";
+street[18]="387 North Lane";
+city[18]="Wheaton";
+state[18]="KY";
+zip[18]="89802";
+amount[18]=250;
+date[18]="2007-09-05";
+
+
+firstName[19]="Arlene";
+lastName[19]="Lutz";
+street[19]="7888 Clear View Drive";
+city[19]="Youngston";
+state[19]="KY";
+zip[19]="88699";
+amount[19]=75;
+date[19]="2007-09-05";
+
+
+firstName[20]="Earl";
+lastName[20]="Holmes";
+street[20]="1001 Rawlings Way";
+city[20]="Jasper";
+state[20]="KY";
+zip[20]="88781";
+amount[20]=500;
+date[20]="2007-09-04";
+
+
+firstName[21]="Bernice";
+lastName[21]="Drew";
+street[21]="25 Main Street";
+city[21]="Drake";
+state[21]="KY";
+zip[21]="88786";
+amount[21]=150;
+date[21]="2007-09-04";
+
+
+firstName[22]="Patrick";
+lastName[22]="Granier";
+street[22]="100 Atwood Avenue";
+city[22]="Delphi";
+state[22]="KY";
+zip[22]="88557";
+amount[22]=75;
+date[22]="2007-09-03";
+
+
+firstName[23]="Henry";
+lastName[23]="Bailey";
+street[23]="37 East Maple Street";
+city[23]="Youngston";
+state[23]="KY";
+zip[23]="89554";
+amount[23]=50;
+date[23]="2007-09-03";
+
+
+firstName[24]="Ginny";
+lastName[24]="Rainey";
+street[24]="657 Dawson Lane";
+city[24]="Youngston";
+state[24]="KY";
+zip[24]="88873";
+amount[24]=50;
+date[24]="2007-09-03";
+
+
+firstName[25]="Ginny";
+lastName[25]="Rainey";
+street[25]="657 Dawson Lane";
+city[25]="Youngston";
+state[25]="KY";
+zip[25]="88873";
+amount[25]=75;
+date[25]="2007-09-03";
+
+
+firstName[26]="Basilia";
+lastName[26]="Lu";
+street[26]="851 Flad Court";
+city[26]="Jasper";
+state[26]="KY";
+zip[26]="88633";
+amount[26]=500;
+date[26]="2007-09-02";
+
+
+firstName[27]="Livia";
+lastName[27]="Mckinnon";
+street[27]="557 Ivy Avenue";
+city[27]="Jasper";
+state[27]="KY";
+zip[27]="88960";
+amount[27]=50;
+date[27]="2007-08-31";
+
+
+firstName[28]="Kris";
+lastName[28]="Levesque";
+street[28]="542 Upton Avenue";
+city[28]="Delphi";
+state[28]="KY";
+zip[28]="88793";
+amount[28]=100;
+date[28]="2007-08-31";
+
+
+firstName[29]="Lynwood";
+lastName[29]="Ingersoll";
+street[29]="723 Jackson Avenue";
+city[29]="Delphi";
+state[29]="KY";
+zip[29]="88802";
+amount[29]=500;
+date[29]="2007-08-30";
+
+
+firstName[30]="Petronila";
+lastName[30]="Damico";
+street[30]="44 Stewart Street";
+city[30]="Drake";
+state[30]="KY";
+zip[30]="88604";
+amount[30]=250;
+date[30]="2007-08-30";
+
+
+firstName[31]="Hugh";
+lastName[31]="Warren";
+street[31]="585 Lindon Court";
+city[31]="Wheaton";
+state[31]="KY";
+zip[31]="88877";
+amount[31]=50;
+date[31]="2007-08-28";
+
+
+firstName[32]="Tom";
+lastName[32]="Thomas";
+street[32]="Rigel Avenue";
+city[32]="Drake";
+state[32]="KY";
+zip[32]="89411";
+amount[32]=100;
+date[32]="2007-08-27";
+
+
+firstName[33]="Steve";
+lastName[33]="Bones";
+street[33]="900 Lawton Street";
+city[33]="Wheaton";
+state[33]="KY";
+zip[33]="89211";
+amount[33]=50;
+date[33]="2007-08-25";
+
+
+firstName[34]="Jeri";
+lastName[34]="White";
+street[34]="Hawkes Lane";
+city[34]="Delphi";
+state[34]="KY";
+zip[34]="89211";
+amount[34]=150;
+date[34]="2007-08-25";

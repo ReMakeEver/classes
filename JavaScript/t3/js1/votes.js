8373cc49fe46e916f5a90f66f59890db375d7384
ReMakeEver <evergenbyremake@gmail.com>
2016-10-27 13:44:45 -0600

    Update votes.js

--- JavaScript/t3/js1/votes.js
+++ JavaScript/t3/js1/votes.js
@@ -1,4 +1,4 @@
-var race = new Array("1st Congresional District","2nd Congressional District","3rd Congressional District","4th Congressional District","5th Congressional District");
+var races = new Array("1st Congresional District","2nd Congressional District","3rd Congressional District","4th Congressional District","5th Congressional District");
 var name1 = new Array("Sara Carlson","Pete deJesus","Alan Tompkins");var party1 = new Array("D","R","I");var votes1 = new Array(45125,4498,5143);
 var name2 = new Array("Gary Griffin","Frank Brown","Ronald Washington","Karen Reese");var party2 = new Array("D","R","G","L");var votes2 = new Array(69505,78133,8109,13004);
 var name3 = new Array("Anne Sanchez","Cynthia Thomas");var party3 = new Array("D","R");var votes3 = new Array(65203,51289);

d9f56b8c9871621ae6b296cc9156ca883563b623
ReMakeEver <evergenbyremake@gmail.com>
2016-11-03 14:39:02 -0600

    new

---
+++ JavaScript/t2/case2/randtxt.js
@@ -0,0 +1,14 @@
+/*
+   New Perspectives on JavaScript
+   Tutorial 2
+   Case Problem 2
+
+   Author: 
+   Date:   
+
+   Function List:
+   randInt(lower, upper)
+      Used to generate a random integer in the range (lower, upper)
+
+*/
+

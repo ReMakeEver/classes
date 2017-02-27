2e5c78c1f08c13959765110ef9da788815480068
ReMakeEver <evergenbyremake@gmail.com>
2017-02-27 13:35:53 -0700

    Update crypt.js

--- crypt.js
+++ crypt.js
@@ -1,20 +1,19 @@
 var crypt = {
- "Encrypt": function(string,times){return encryptString(string);},
- "Decrypt": function(string){decryptString(string);}
+ "Encrypt": function(string,times){return encryptString(string,times);},
+ "Decrypt": function(string,level){decryptString(string,level);}
 };
 function encryptString(string,times){
  var data = "";
- if(times==null){
+ if(times==null || times==1){
   for(var i = 0;i<string.length;i++){
    data += string.charCodeAt(i);
   }
  }else{
   if(times==2){
-   var string2 = "";
    for(var i = 0;i<string.length;i++){
-    string2 += string.charCodeAt(i);
+    data += string.charCodeAt(i);
    }
-   return level2En(string2);
+   return level2En(data);
   }
  }
  return data;
@@ -26,11 +25,28 @@ function level2En(string){
  }
  return data;
 }
-function decryptString(string){
+function decryptString(string,level){
  var split = string.split(",");
  var data = "";
- for(var i = 0;i<split.length;i++){
-  data += String.fromCharCode(split[i]);
+ if(level==null || level==1){
+  for(var i = 0;i<split.length;i++){
+   data += String.fromCharCode(split[i]);
+  }
+ }else{
+  if(level==2){
+   for(var i = 0;i<split.length;i++){
+    data += String.fromCharCode(split[i])+",";
+   }
+   return level2De(data);
+  }
  }
  return data;
 }
+ function level2De(string){
+  var split = string.split(",");
+  var data = "";
+  for(var i = 0;i<split.length;i++){
+   data += String.fromCharCode(split[i]);
+  }
+  return data;
+ }
